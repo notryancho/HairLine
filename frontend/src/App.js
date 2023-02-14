919fc1a7fa9a20e04b29ae7f82ad7e264fa4fed6
notryancho <ryancho1226@hotmail.com>
2023-02-14 15:32:26 -0500

    pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import {Route, Routes} from 'react-router-dom'
 import './styles/app.css';
 import Home from './pages/Home';
+import About from './pages/About';
 
 const App = () => {
     return (
         <div className="App">
-            <Home />
+            <main>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    {/* <Route path="/search" element={<Search />} />
+                    <Route path="/browse" element={<Browse />} /> */}
+                </Routes>
+            </main>
+            
         </div>
     )
 }
