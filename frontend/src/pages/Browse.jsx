bfb669db112a857da3c19e6859000c3926949872
notryancho <ryancho1226@hotmail.com>
2023-02-15 22:59:49 -0500

    browse images

--- frontend/src/pages/Browse.jsx
+++ frontend/src/pages/Browse.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 import Nav from '../components/Nav';
 // import { Card, Col, Row } from 'react-bootstrap';
 
+
 const Browse = () => {
   return (
     <div>
       {window.location.pathname !== '/' && <Nav />}
       <h1>Browse Hairstyles</h1>
-      <img src="./hairstyles/afrolong.png" alt="thick hairstyle"></img>
+      <div className="gallery">
+      
+      <img src="https://i.imgur.com/udyC7em.png" alt="wavyshort" />
+      <img src="https://i.imgur.com/jDi5otR.png" alt="wavymed" />
+      <img src="https://i.imgur.com/EHGBaX4.png" alt="wavylong" />
+      <img src="https://i.imgur.com/5N1jlIt.png" alt="thinshort" />
+      <img src="https://i.imgur.com/8Ei9sWu.png" alt="thinmedd" />
+      <img src="https://i.imgur.com/BA2afIo.png" alt="thinmed" />
+      <img src="https://i.imgur.com/NlerdLe.png" alt="thickshort" />
+      <img src="https://i.imgur.com/0iR4MK4.png" alt="thickmed" />
+      <img src="https://i.imgur.com/TwPs86T.png" alt="thicklong" />
+      <img src="https://i.imgur.com/HmPs0RD.png" alt="fineshort" />
+      <img src="https://i.imgur.com/7mODHpU.png" alt="finemed" />
+      <img src="https://i.imgur.com/7MtPC4h.png" alt="finelong" />
+      <img src="https://i.imgur.com/eTIWAsx.png" alt="afroshort" />
+      <img src="https://i.imgur.com/Vcldxp0.png" alt="afromed" />
+      <img src="https://i.imgur.com/C1gFMpj.png" alt="afrolong" />
+
+      </div>
     </div>
 
   );
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,8 +1,8 @@
-// import React from "react";
+import React from "react";
 import Nav from "../components/Nav";
 // import ImageGrid from "../components/ImageGrid";
 
-const Search = () => {
+const Search = () => { 
   // const [selectedTag, setSelectedTag] = useState("");
 
   const handleTagClick = (tag) => {
